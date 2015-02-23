dfc8bff29f8b7c0028ffcc6ba4f3d2ba46835e0d
Adam Rensel <adamrensel@gmail.com>
2015-02-23 14:18:11 -0500

    Remove all prior assets and reoganize files a bit to be cleaner

--- app/server/modules/expressConfig.js
+++ app/server/modules/expressConfig.js
@@ -2,10 +2,10 @@ var bodyParser = require('body-parser');
 
 module.exports = function(app, express) {
   // Serve static assets from the app folder. This enables things like javascript
-  // and stylesheets to be loaded as expected. You would normally use something like 
+  // and stylesheets to be loaded as expected. You would normally use something like
   // nginx for this normally, but this makes for a simpler demo app to just let express do it.
   app.use("/", express.static("app/"));
-  app.set('views', __dirname + '/../views'); // Set the view directory, this enables us to use the .render method inside routes
+  app.set('views', __dirname + '/../../views'); // Set the view directory, this enables us to use the .render method inside routes
   app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
   app.use(bodyParser.json()); // parse application/json
 }
